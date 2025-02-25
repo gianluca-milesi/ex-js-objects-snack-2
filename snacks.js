f9f9d802b782301f0a17524c37bfb694fefc0a6c
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-25 12:48:27 +0100

    ADD: Snack 1

---
+++ snacks.js
@@ -0,0 +1,8 @@
+//SNACK 1
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); //Double Cheese Burger
+console.log(secondBurger.name); //Double Cheese Burger
