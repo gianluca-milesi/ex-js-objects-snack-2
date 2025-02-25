da798fd9de1aaa2d74c285f0a5ce20adc7426f21
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-25 21:05:17 +0100

    Update: Correzioni

--- snacks.js
+++ snacks.js
@@ -7,6 +7,9 @@ secondBurger.weight = 500;
 console.log(hamburger.name); //Double Cheese Burger
 console.log(secondBurger.name); //Double Cheese Burger
 
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+//1
+
 
 //SNACK 2
 const hamburger = {
@@ -20,6 +23,9 @@ secondBurger.ingredients[0] = "Salad";
 
 console.log(hamburger.ingredients[0]); //Salad
 console.log(secondBurger.ingredients[0]); //Salad
+
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+//2
 
 
 //SNACK 3
@@ -91,10 +97,13 @@ const secondBurger = { ...hamburger };
 secondBurger.maker.restaurant = newRestaurant;
 secondBurger.maker.name = "Chef Hyur";
 
-console.log(hamburger.maker.name); //Anonymous Chef
+console.log(hamburger.maker.name); //Chef Hyur
 console.log(secondBurger.maker.name); //Chef Hyur
-console.log(hamburger.maker.restaurant.name); //Hyur's Burgers
+console.log(hamburger.maker.restaurant.name); //Hyur's II
 console.log(secondBurger.maker.restaurant.name); //Hyur's II
+
+//Quanti oggetti sono stati creati in memoria?
+//5
 
 
 //SNACK 6
@@ -122,20 +131,34 @@ const chef = {
 
 //Qual è il metodo migliore per clonare l’oggetto chef, e perché?
 //Nessun metodo visto è il migliore ma si può usare una funzione ricorsiva o _.cloneDeep(obj) (Lodash)
+const chefCopy = {
+    ...chef,
+    restaurant: {
+        ...chef.restaurant,
+        address: {
+            ...chef.restaurant.address
+        }
+    }
+}
+console.log(chefCopy)
 
 
 //SNACK (Bonus)
-function deepClone(oggetto) {
-    if (typeof oggetto !== "object" || oggetto === null) return oggetto
+function deepCopy(obj) {
+    if (typeof obj !== "object" || obj === null) return obj
 
-    const nuovoOggetto = Array.isArray(oggetto) ? [] : {}
+    const newObj = {}
 
-    for (let chiave in oggetto) {
-        nuovoOggetto[chiave] = deepClone(oggetto[chiave])
+    for (let key in obj) {
+        const value = obj[key]
+        if (typeof value !== "object") {
+            newObj[key] = value
+        } else {
+            newObj[key] = deepCopy(value)
+        }
     }
-
-    return nuovoOggetto
+    return newObj
 }
 
-const nuovoChef = deepClone(chef)
+const nuovoChef = deepCopy(chef)
 console.log(nuovoChef)
